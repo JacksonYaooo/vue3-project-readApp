cfaab03f6221ebc9ba32f55eb68f1c26a18f732e
huangmengyao <2594838054@qq.com>
2023-11-08 22:10:47 +0800

    feat: 排行榜内容区域的模块划分

--- src/views/index/chartstab.vue
+++ src/views/index/chartstab.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="chartstab">
     <van-tabs v-model:active="active">
-      <van-tab :title="v.title" v-for="(v,i) in chartstab" :key="i">{{ v.content }}</van-tab>
+      <van-tab :title="v.title" v-for="(v,i) in chartstab" :key="i">
+        <div class="chartstab-content">
+          <div class="chartstab-item">
+            <div class="chartstab-list" v-for="(item,i) in v.content" :key="i">
+              {{ item.name }}
+            </div>
+          </div>
+        </div>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
@@ -11,12 +19,12 @@ import { ref } from 'vue';
 
 const active = ref(0);
 const chartstab = ref([
-  {title:'全部',content:'内容1'},
-  {title:'都市',content:'内容2'},
-  {title:'历史',content:'内容3'},
-  {title:'科幻',content:'内容4'},
-  {title:'全部',content:'内容5'},
-  {title:'都市',content:'内容6'},
+  {title:'全部',content:[{name:'内容1',},{name:'内容a',},{name:'内容b',},{name:'内容1A',},{name:'内容Aa',},{name:'内容Ab',},]},
+  {title:'都市',content:[{name:'内容1',},{name:'内容a',},{name:'内容b',},]},
+  {title:'历史',content:[{name:'内容1',},{name:'内容a',},{name:'内容b',},]},
+  {title:'科幻',content:[{name:'内容1',},{name:'内容a',},{name:'内容b',},]},
+  {title:'全部',content:[{name:'内容1',},{name:'内容a',},{name:'内容b',},]},
+  {title:'都市',content:[{name:'内容1',},{name:'内容a',},{name:'内容b',},]},
 ])
 </script>
 
