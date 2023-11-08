b780b88562ff2ceaea3becebcd978f69b5ba0876
huangmengyao <2594838054@qq.com>
2023-11-08 17:41:18 +0800

    feat: 选项卡组件

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,13 +2,15 @@
   <div>
     <Header></Header>
     <Charts></Charts>
+    <Chartstab/>
   </div>
 </template>
 
 <script setup>
- import {ref} from 'vue'
- import Header from './header.vue'
- import Charts from './charts.vue'
+  import {ref} from 'vue'
+  import Header from './header.vue'
+  import Charts from './charts.vue'
+  import Chartstab from './chartstab.vue';
 </script>
 
 <style lang='scss' scoped>
