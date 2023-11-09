19f7889bf3c058ffcdd2053566b440d5bb75dc4a
huangmengyao <2594838054@qq.com>
2023-11-09 22:40:28 +0800

    feat: 增加社区页面结构

--- src/views/community/index.vue
+++ src/views/community/index.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    社区
+  <div class="community">
+    <div class="community-hd">
+      <div class="community-hd-item">
+        <h3>推荐</h3>
+      </div>
+    </div>
+    <div class="community-body">
+      <div class="community-body-item">
+        <div class="community-item-hd">
+          <img src="@/assets/images/h.jpg" alt="">
+          <p>小说圈</p>
+          <span>作者名</span>
+          <div class="community-share">
+            <i class="iconfont icon-gengduo"></i>
+          </div>
+        </div>
+        <div class="community-item-title">
+          为什么会这样?
+        </div>
+        <div class="community-item-img">
+          图片
+        </div>
+        <div class="community-item-review">
+          图标
+        </div>
+      </div>
+      
+    </div>
+    
   </div>
 </template>
 
